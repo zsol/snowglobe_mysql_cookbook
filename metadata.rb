546736125fe520b0601e5d34977798ab462f3438
Zsolt Dollenstein <zsol.zsol@gmail.com>
2015-02-01 22:00:27 +0100

    add powerdns db config

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'snowglobe_mysql'
 license          'All rights reserved'
 description      'Installs/Configures snowglobe_mysql'
 long_description 'Installs/Configures snowglobe_mysql'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'mysql', "~> 5.6"
--- recipes/default.rb
+++ recipes/default.rb
@@ -18,3 +18,42 @@ self.class.send(:include, Opscode::OpenSSL::Password)
 node.set_unless['mysql']['server_root_password'] = secure_password
 node.set_unless['mysql']['server_debian_password'] = secure_password
 node.set_unless['mysql']['server_repl_password'] = secure_password
+
+include_recipe "mysql::server"
+
+node.set_unless['pdns']['authoritative']['config']['gmysql_password'] = secure_password
+
+pdns_config = node['pdns']['authoritative']['config']
+
+connection = {
+  :host => 'localhost',
+  :username => 'root',
+  :password => node['mysql']['server_root_password'],
+}
+
+mysql_database pdns_config['gmysql_dbname'] do
+  connection connection
+  action :create
+  notifies :query, "mysql_database[create pdns tables]"
+end
+
+mysql_database_user pdns_config['gmysql_user'] do
+  connection connection
+  password pdns_config['gmysql_password']
+  database_name pdns_config['gmysql_dbname']
+  privileges [:all]
+  action [:create, :grant]
+end
+
+schema_file = "/etc/powerdns_mysql_schema.sql"
+
+cookbook_file schema_file do
+  source "powerdns_schema.sql"
+end
+
+mysql_database "create pdns tables" do
+  database_name pdns_config['gmysql_dbname']
+  connection connection
+  sql { ::File.open(schema_file).read }
+  action :nothing
+end
